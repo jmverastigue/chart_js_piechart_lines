bee0a51d0d73ecca8ad20688a2b9000f4a166ddd
Jm Verastigue <jm.verastigue@yahoo.com>
2016-03-22 10:47:57 +0800

    Initial Commit
    Creates labels outside the pie chart of chart.js
    Note: Canvas is set to 500px.

---
+++ chartjs_options.js
@@ -0,0 +1,56 @@
+var line_counter = 0;
+var near_y_right = [];
+var near_y_left = [];
+
+var options = {
+    tooltipTemplate: "<%=label%> <%= value %>%",
+    onAnimationComplete: function() {
+        this.showTooltip(this.segments, true);
+    },
+    tooltipEvents: [],
+    customTooltips: function(tooltip) {
+        line_counter++;
+        if (!tooltip) {
+            return;
+        }
+        else{
+
+          if(tooltip.x > 250){
+
+            near_y_right.forEach(function(y_axis){
+              if((tooltip.y >= y_axis - 10) && (tooltip.y <= y_axis + 10)){
+                tooltip.y -= 20;
+              }
+            });
+            near_y_right[line_counter] = tooltip.y;
+
+            tooltip.chart.ctx.beginPath();
+            tooltip.chart.ctx.strokeStyle = '#000';
+            tooltip.chart.ctx.lineWidth=1;
+            tooltip.chart.ctx.moveTo(tooltip.x,tooltip.y);
+            tooltip.chart.ctx.lineTo(400,tooltip.y);
+            tooltip.chart.ctx.stroke();
+            tooltip.chart.ctx.textAlign="left";
+            tooltip.chart.ctx.fillText(tooltip.text,403,tooltip.y+5);
+          }
+          else{
+
+            near_y_left.forEach(function(y_axis){
+              if((tooltip.y >= y_axis - 10) && (tooltip.y <= y_axis + 10)){
+                tooltip.y -= 20;
+              }
+            });
+            near_y_left[line_counter] = tooltip.y;
+
+            tooltip.chart.ctx.beginPath();
+            tooltip.chart.ctx.strokeStyle = '#000';
+            tooltip.chart.ctx.moveTo(tooltip.x,tooltip.y);
+            tooltip.chart.ctx.lineTo(100,tooltip.y);
+            tooltip.chart.ctx.stroke();
+            tooltip.chart.ctx.textAlign="right";
+            tooltip.chart.ctx.fillText(tooltip.text,95,tooltip.y+5);
+          }
+        }
+    },
+    showTooltips: false
+};
