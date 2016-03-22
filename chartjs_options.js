f84b87a778384186199afd1ef4ed60eba26bba31
Jm Verastigue <jm.verastigue@yahoo.com>
2016-03-22 12:47:13 +0800

    Update chartjs_options.js

--- chartjs_options.js
+++ chartjs_options.js
@@ -1,7 +1,6 @@
 var line_counter = 0;
 var near_y_right = [];
 var near_y_left = [];
-
 var options = {
     tooltipTemplate: "<%=label%> <%= value %>%",
     onAnimationComplete: function() {
@@ -14,9 +13,7 @@ var options = {
             return;
         }
         else{
-
-          if(tooltip.x > 250){
-
+          if(tooltip.x > (tooltip.chart.width/2)){
             near_y_right.forEach(function(y_axis){
               if((tooltip.y >= y_axis - 10) && (tooltip.y <= y_axis + 10)){
                 tooltip.y -= 20;
@@ -28,13 +25,12 @@ var options = {
             tooltip.chart.ctx.strokeStyle = '#000';
             tooltip.chart.ctx.lineWidth=1;
             tooltip.chart.ctx.moveTo(tooltip.x,tooltip.y);
-            tooltip.chart.ctx.lineTo(400,tooltip.y);
+            tooltip.chart.ctx.lineTo(tooltip.chart.width - 100,tooltip.y);
             tooltip.chart.ctx.stroke();
             tooltip.chart.ctx.textAlign="left";
-            tooltip.chart.ctx.fillText(tooltip.text,403,tooltip.y+5);
+            tooltip.chart.ctx.fillText(tooltip.text,(tooltip.chart.width - 100) + 3,tooltip.y+5);
           }
           else{
-
             near_y_left.forEach(function(y_axis){
               if((tooltip.y >= y_axis - 10) && (tooltip.y <= y_axis + 10)){
                 tooltip.y -= 20;
